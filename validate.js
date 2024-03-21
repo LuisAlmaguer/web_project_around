2ee323594a47b408ec2c15858b6cee0f4a0fca0c
Almaguer Torres <luis.almaguertorres@carrier.com>
2024-03-21 07:53:52 -0600

    refactor: BEM buton

--- validate.js
+++ validate.js
@@ -69,7 +69,7 @@ const toggleButtonState = (inputList, buttonElement, settings) => {
     formSelector: ".popup__form",
     inputSelector: ".popup__input",
     submitButtonSelector: ".popup__submit",
-    inactiveButtonClass: "popup__button_disabled",
+    inactiveButtonClass: "popup__button-disabled",
     inputErrorClass: "popup__input_type_error",
     errorClass: "popup__error_visible"
   });
