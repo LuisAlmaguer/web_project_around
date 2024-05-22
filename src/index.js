286e73fbd2af3ac0839dd7dda1bc40e2c3f46380
LuisAlmaguer <almaguer.luis@hotmail.com>
2024-05-22 16:35:40 -0600

    webpack

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ import{
 
 // variables
 
+const numbers = [2, 3, 5];
+
+// Función arrow. ¿Cómo se las arreglará Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2); 
+
+console.log(doubledNumbers); // 4, 6, 10
+
 const initialCards = [
   {
     name: "Valle de Yosemite",
